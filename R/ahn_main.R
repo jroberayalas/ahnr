9b9992beb7f60a770432eaa754f715109e93484f
José Roberto Ayala Solares <ichbinjras@gmail.com>
2017-05-13 19:10:28 +0100

    Functions implemented

---
+++ R/ahn_main.R
@@ -0,0 +1,182 @@
+#' AHNnD
+#'
+#' @param Sigma a list with two data frames. One for the inputs X, and one for the outputs Y.
+#' @param n number of particles to use.
+#' @param eta learning rate of the algorithm. Default is \code{0.01}.
+#' @param maxIter maximum number of iterations.
+#'
+#' @return a list with the following components:
+#' \itemize{
+#'         \item network: structure of the AHN trained.
+#'         \item Yo: original output variable.
+#'         \item Ym: predicted output variable.
+#' }
+#' @export
+#'
+#' @examples
+#' # Create data
+#' x <- 2 * runif(1000) - 1;
+#' x <- sort(x)
+#'
+#' y <- (x < 0.1) * (0.05 * runif(100) + atan(pi*x)) +
+#'     (x >= 0.1 & x < 0.6) * (0.05 * runif(1000) + sin(pi*x)) +
+#'     (x >= 0.6) * (0.05 * runif(1000) + cos(pi*x))
+#'
+#' # Create Sigma list
+#' Sigma <- list(X = data.frame(x = x), Y = data.frame(y = y))
+#'
+#' # Train AHN
+#' ahn <- AHNnD(Sigma, 5, 0.01, 2000)
+#'
+AHNnD <- function(Sigma, n, eta, maxIter = 2000) {
+    maxIter <- maxIter
+    iter <- 1
+
+    X <- Sigma$X
+    Y <- Sigma$Y
+
+    newIdx <- sort(Y[ , 1], index.return = TRUE)$ix
+    X <- X[newIdx, , drop = FALSE]
+    Y <- Y[newIdx, , drop = FALSE]
+
+    numOutputs <- ncol(Y)
+
+    overallError <- rep(Inf, maxIter)
+    minOverallError <- Inf
+
+    # Create a saturated and linear compound
+    C <- CreateLinearCompound(n)
+
+    # Initialize randomly the position of molecules
+    posMolecules <- ahn_Pi <- InitialPosition(X, n)
+
+    # Initialize AHN-structure
+    H.names <- paste("molecule", 1:n, sep = "")
+    H <- vector("list", length(H.names))
+    names(H) <- H.names
+    ahn_H <- H
+
+    # Train compound
+    while (iter < maxIter) {
+        requireRelocation <- TRUE
+        relocationIter <- 1
+        maxRI <- 10
+
+        while (requireRelocation & (relocationIter < maxRI)) {
+            molecules <- DataInMolecules(Sigma, posMolecules)
+
+            Yo <- Ym <- matrix(0, nrow = nrow(Y), ncol = ncol(Y))
+            pointer <- 1
+
+            errorMolecule <- matrix(0, nrow = n+1, ncol = numOutputs)
+
+            for (i in seq_len(length(molecules$SigmaSplit$X))) {
+                Xi <- molecules$SigmaSplit$X[[i]]
+                Yi <- molecules$SigmaSplit$Y[[i]]
+
+                parameters <- ComputeMoleculeParameters(Xi, Yi, C$Omega[molecules$moleculesUsed[i]])
+
+                H[[names(molecules$SigmaSplit$X)[i]]] <- parameters$H
+
+                errorMolecule[molecules$moleculesUsed[i], ] <- parameters$errorMolecule
+
+                pointerNew <- pointer + nrow(parameters$Yapprox)
+                Yo[pointer:(pointerNew-1), ] <- Yi[seq_len(nrow(Yi)), ]
+                Ym[pointer:(pointerNew-1), ] <- parameters$Yapprox
+                pointer <- pointerNew
+            }
+
+            relocation <- RelocateMolecules(posMolecules, molecules$moleculesUsed, errorMolecule)
+            requireRelocation <- relocation$requireRelocation
+            posMolecules <- relocation$posMolecules
+            relocationIter <- relocationIter + 1
+        }
+
+        if (relocationIter >= maxIter) {
+            H <- ahn_H
+            posMolecules <- ahn_Pi
+        }
+
+        # Update AHN-structure
+        overallError[iter] <- sum(errorMolecule)
+
+        if (overallError[iter] < minOverallError) {
+            ahn_H <- H
+            ahn_Pi <- posMolecules
+            minOverallError = overallError[iter]
+        }
+
+        # Update intermolecular distances
+        for (i in seq_len(n)) {
+            deltaPosMolecule <- -eta * (sum(errorMolecule[i, ]) - sum(errorMolecule[i+1, ])) * rep(1, ncol(posMolecules))
+            posMolecules[i, ] <- posMolecules[i, ] + deltaPosMolecule
+        }
+
+        iter <- iter + 1
+    }
+    network = list(H = ahn_H, Pi = ahn_Pi, n = n, C = C)
+    list(network = network, Yo = Yo, Ym = Ym)
+}
+
+
+#' SimAHNnD
+#'
+#' @param ahn a list produced from the \link{AHNnD} function.
+#' @param X data frame with the inputs X to be predicted.
+#'
+#' @return predicted output values for inputs X.
+#' @export
+#'
+#' @examples
+#' # Create data
+#' x <- 2 * runif(1000) - 1;
+#' x <- sort(x)
+#'
+#' y <- (x < 0.1) * (0.05 * runif(100) + atan(pi*x)) +
+#'     (x >= 0.1 & x < 0.6) * (0.05 * runif(1000) + sin(pi*x)) +
+#'     (x >= 0.6) * (0.05 * runif(1000) + cos(pi*x))
+#'
+#' # Create Sigma list
+#' Sigma <- list(X = data.frame(x = x), Y = data.frame(y = y))
+#'
+#' # Train AHN
+#' ahn <- AHNnD(Sigma, 5, 0.01, 2000)
+#'
+#' # Test AHN
+#' X <- data.frame(x = x)
+#' ysim <- SimAHNnD(ahn, X)
+#'
+SimAHNnD <- function(ahn, X) {
+    # Extract network components
+    H <- ahn$network$H
+    posMolecules <- ahn$network$Pi
+    C <- ahn$network$C
+
+    # Initial statemets
+    Yapprox <- matrix(0, nrow = nrow(X), ncol = ncol(H[[1]]))
+    indexes <- rep(0, nrow(X))
+
+    # Distribute data over molecules
+    molecules <- SimDataInMolecules(X, posMolecules)
+
+    # Evaluate AHN-model
+    pointer <- 1
+
+    for (i in seq_len(length(molecules$X))) {
+        Xi <- molecules$X[[i]]
+        indexesi <- molecules$Index[[i]]
+
+        ki <- C$Omega[i]
+        Phi <- CH_X(Xi, ki)
+        Ym <- Phi %*% H[[i]]
+
+        pointerNew <- pointer + nrow(Ym)
+        Yapprox[pointer:(pointerNew-1), ] <- Ym#[seq_len(nrow(Ym)), ]
+        indexes[pointer:(pointerNew-1)] <- indexesi
+        pointer <- pointerNew
+    }
+
+    # Sort data
+    ix <- sort(indexes, index.return = TRUE)$ix;
+    Y <- Yapprox[ix, ]
+}
---
+++ R/auxiliary.R
@@ -0,0 +1,145 @@
+# CH_X creates the Vandermonde matrix
+CH_X <- function(X, h) {
+    N <- ncol(X)
+    q <- nrow(X)
+
+    Phi <- matrix(0, nrow = q)
+
+    for (i in seq_len(N)) {
+        Phi <- cbind(Phi, matrixcalc::vandermonde.matrix(X[ , i], h+1))
+    }
+    Phi <- Phi[ , -1]
+    Phi
+}
+
+# ComputeMoleculeParameters solves the system of equations with Least Squares
+ComputeMoleculeParameters <- function(Xi, Yi, Omegai) {
+    Phi <- CH_X(Xi, Omegai)
+
+    if (is.vector(Phi)) {
+        Phi <- matrix(Phi, nrow = 1)
+    }
+
+    H <- pracma::mldivide(Phi, as.matrix(Yi))
+    Yapprox <- Phi %*% H
+
+    errorMolecule <- sqrt(mean(abs((Yapprox - Yi)^2)))
+
+    list(H = H, Yapprox = Yapprox, errorMolecule = errorMolecule)
+}
+
+# CreateLinearCompound creates a network with n molecules
+CreateLinearCompound <- function(n) {
+    Omega <- 2 * rep(1, n)
+    Omega[1] <- 3
+    Omega[length(Omega)] <- 3
+
+    B <- rep(1, n-1)
+
+    list(Omega = Omega, B = B)
+}
+
+# InitialPosition defines the initial positions of the n molecules
+InitialPosition <- function(X, n) {
+    minValues <- purrr::map_dbl(X, min)
+    maxValues <- purrr::map_dbl(X, max)
+
+    ranges <- maxValues - minValues
+
+    rangesMatrix <- matrix(ranges, nrow = n, ncol = length(ranges), byrow = TRUE)
+    rUnifMatrix <- matrix(runif(n*length(ranges)), nrow = n, ncol = length(ranges), byrow = TRUE)
+    minMatrix <- matrix(minValues, nrow = n, ncol = length(ranges), byrow = TRUE)
+    posMolecules <- rangesMatrix * rUnifMatrix + minMatrix
+
+    ###
+    # rangesMatrix[1, ] <- minValues + (ranges / (n * 2))
+    # for (k in seq_len(n-1)) {
+    #     rangesMatrix[(k+1), ] <- minValues + (ranges * (2 * k + 1)/ (n * 2))
+    # }
+    # posMolecules <- rangesMatrix
+    ###
+
+    posMolecules
+}
+
+# DataInMolecules groups the data to the closer molecule
+DataInMolecules <- function(Sigma, posMolecules) {
+    X <- Sigma$X
+    Y <- Sigma$Y
+
+    numSamples <- nrow(X)
+    n <- nrow(posMolecules)
+
+    # dist <- matrix(0, nrow = numSamples, ncol = n)
+    #
+    # for (i in seq_len(n)) {
+    #     for (j in seq_len(numSamples)) {
+    #         dist[j, i] <- Distance(X[j, ], posMolecules[i, ])
+    #     }
+    # }
+
+    dist <- as.matrix(pdist::pdist(as.matrix(X), posMolecules))
+
+    moleculeNumber <- factor(paste("molecule", apply(dist, 1, which.min), sep = ""))
+
+    SigmaSplitX <- split(X, moleculeNumber)
+    SigmaSplitY <- split(Y, moleculeNumber)
+    SigmaSplit <- list(X = SigmaSplitX, Y = SigmaSplitY)
+    SigmaSplit
+
+    list(SigmaSplit = SigmaSplit, moleculesUsed = sort(unique(apply(dist, 1, which.min))))
+}
+
+# RelocateMolecules relocates molecules with no data
+RelocateMolecules <- function(posMolecules, moleculesUsed, errorMolecule) {
+    n <- nrow(posMolecules)
+
+    errorMolecules <- errorMolecule[moleculesUsed, , drop = FALSE]
+    moleculeNumber <- sort(apply(errorMolecules, 1, sum), decreasing = TRUE, index.return = TRUE)$ix
+
+    requireRelocation <- !(nrow(errorMolecules) == n)
+
+    if (requireRelocation) {
+        seekMolecule <- 1
+
+        for (i in seq_len(n)) {
+            if (!(i %in% moleculesUsed)) {
+                radius <- 1
+                posMolecules[i, ] <- rep(radius, ncol(posMolecules)) * runif(ncol(posMolecules)) + posMolecules[moleculeNumber[seekMolecule], ]
+
+                seekMolecule <- seekMolecule + 1
+
+                if (seekMolecule > length(moleculeNumber)) {
+                    seekMolecule <- 1
+                }
+            }
+        }
+    }
+
+    list(requireRelocation = requireRelocation, posMolecules = posMolecules)
+}
+
+# SimDataInMolecules groups the data to the closer molecule based on the trained network
+SimDataInMolecules <- function(X, posMolecules) {
+    numSamples <- nrow(X)
+    n <- nrow(posMolecules)
+
+    index <- 1:numSamples
+
+    # dist <- matrix(0, nrow = numSamples, ncol = n)
+    #
+    # for (i in seq_len(n)) {
+    #     for (j in seq_len(numSamples)) {
+    #         dist[j, i] <- Distance(X[j, ], posMolecules[i, ])
+    #     }
+    # }
+
+    dist <- as.matrix(pdist::pdist(as.matrix(X), posMolecules))
+
+    moleculeNumber <- factor(paste("molecule", apply(dist, 1, which.min), sep = ""))
+
+    SigmaSplitX <- split(X, moleculeNumber)
+    SigmaSplitIndex <- split(index, moleculeNumber)
+    SigmaSplit <- list(X = SigmaSplitX, Index = SigmaSplitIndex)
+    SigmaSplit
+}
