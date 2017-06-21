94532eed5eae2862f0c693f237ab5652aadfdedb
José Roberto Ayala Solares <ichbinjras@gmail.com>
2017-06-21 15:25:25 +0100

    Delete ggraph dependecies

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,5 @@
 #' @importFrom stats runif
 #' @importFrom ggplot2 aes theme_void
-#' @importFrom ggraph ggraph geom_edge_link geom_node_text label_rect
 #' @importFrom visNetwork visNetwork visGroups visLegend
 #' @importFrom magrittr %>%
 NULL
